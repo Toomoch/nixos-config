c16aab1c7396505b6ba9c661d3d1186a784f9203
Arnau Valls Fusté <vallsfustearnau@gmail.com>
2023-10-02 10:56:28 +0200

    ª

--- home/arnau/default.nix
+++ home/arnau/default.nix
@@ -21,7 +21,7 @@ in
     bashrcExtra = ''
       eval "$(starship init bash)"
       function set_win_title(){
-        echo -ne "\033]0; $(PWD) \007"
+        echo -ne "\033]0; $PWD \007"
       }
       starship_precmd_user_func="set_win_title"
     '';
