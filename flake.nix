9af5e22aa8f7f13d80ce70e45282dc62c07a5baf
Toomoch <vallsfustearnau@gmail.com>
2023-10-25 20:32:20 +0200

    test

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
         desktop = import ./home/arnau/desktop.nix;
       };
 
-      nixosModules.common = import ./system/modules;
+      nixosModules.common = import ./system/modules { inherit inputs; }; 
 
       nixosConfigurations = {
         b450 = nixpkgs-unstable.lib.nixosSystem {
