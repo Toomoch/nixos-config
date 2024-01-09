e52a9c54d46eeed203ee775817afc1bc4bf4bdbe
Toomoch <vallsfustearnau@gmail.com>
2024-01-09 23:30:00 +0100

    solve ssh issue in fhs + rework readme

--- home/arnau/default.nix
+++ home/arnau/default.nix
@@ -82,4 +82,9 @@ in
     ];
   };
 
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
+
 }
