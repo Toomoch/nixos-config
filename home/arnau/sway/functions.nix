b0c48592ab90a8461c3a376f7e066a33f0fbbd01
Toomoch <vallsfustearnau@gmail.com>
2024-02-19 00:23:03 +0100

    fix workspace assign

--- home/arnau/sway/functions.nix
+++ home/arnau/sway/functions.nix
@@ -6,5 +6,5 @@
     let
       size = end - begin + 1;
     in
-      lib.lists.imap1 (i: v: "${pkgs.sway}/bin/swaymsg workspace ${toString (i + begin - 1)}, move workspace to output \'\"${monitor}\"\'") (lib.lists.replicate size "");
+      lib.lists.imap1 (i: v: "${pkgs.sway}/bin/swaymsg workspace ${toString (i + begin - 1)} output \'\"${monitor}\"\' && ${pkgs.sway}/bin/swaymsg workspace ${toString (i + begin - 1)}, move workspace to output \'\"${monitor}\"\'") (lib.lists.replicate size "");
 }
