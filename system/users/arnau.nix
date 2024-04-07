fef4ba828367bc159553398ded5005c35c014666
Toomoch <vallsfustearnau@gmail.com>
2024-04-07 20:47:54 +0200

    Add sshagent

--- system/users/arnau.nix
+++ system/users/arnau.nix
@@ -14,6 +14,7 @@
   };
   programs.zsh.enable = true;
 
-
+  security.pam.enableSSHAgentAuth = true;
+  security.pam.services.sudo.sshAgentAuth = true;
   nix.settings.trusted-users = [ "arnau" ];
 }
