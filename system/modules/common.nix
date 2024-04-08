9682fa8b12628a4ac2ffa3d92a7a94598d6f9dea
Toomoch <vallsfustearnau@gmail.com>
2024-04-08 23:12:46 +0200

    Add fwupd

--- system/modules/common.nix
+++ system/modules/common.nix
@@ -74,7 +74,8 @@ in
         just
         dig
       ];
-
+      
+      services.fwupd.enable = true;
       # Some programs need SUID wrappers, can be configured further or are
       # started in user sessions.
       # programs.mtr.enable = true;
