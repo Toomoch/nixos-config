512e9fd638fe6e3d70090261800e505153ea96c5
Toomoch <vallsfustearnau@gmail.com>
2024-04-17 19:26:31 +0200

    DNS nixondroid

--- home/arnau/machine/nix-on-droid.nix
+++ home/arnau/machine/nix-on-droid.nix
@@ -1,11 +1,20 @@
 { config, lib, pkgs, ... }:
-
+let
+  dnshack = pkgs.callPackage (builtins.fetchTarball "https://github.com/ettom/dnshack/tarball/master") { };
+  variables = {
+    DNSHACK_RESOLVER_CMD = "${dnshack}/bin/dnshackresolver";
+    LD_PRELOAD = "${dnshack}/lib/libdnshackbridge.so";
+  };
+in
 {
   # Read the changelog before changing this value
   home.stateVersion = "23.11";
-  imports = [ 
+  imports = [
     ../default.nix
-    ../personal.nix 
+    ../personal.nix
   ];
+
+  programs.bash.sessionVariables = variables;
+  programs.zsh.sessionVariables = variables;
   # insert home-manager config
 }
--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -9,24 +9,23 @@
     git
     neofetch
     zsh
-    findutils
     procps
     # Some common stuff that people expect to have
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
