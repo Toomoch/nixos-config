8857e12637cb7ec41d1c0f4cf5233ca28283d5a8
Arnau <arnau@nixos.>
2024-09-03 17:43:12 +0200

    nixos

--- system/machine/work/disko.nix
+++ system/machine/work/disko.nix
@@ -1,39 +1,32 @@
 {
+  # checkout the example folder for how to configure different disko layouts
   disko.devices = {
-    disk = {
-      vdb = {
-        device = "/dev/disk/by-id/ata-CT500MX500SSD1_1822E14161FB";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              name = "ESP";
-              type = "EF00";
-              size = "500M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "500M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-	    swap = {
-	      type = "8200";
-              size = "8G";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true; # resume from hiberation from this device
-              };
-            };
-            root = {
-              name = "nixos";
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "btrfs";
-                mountpoint = "/";
-              };
+          };
+          swap = {
+            type = "8200";
+            size = "2G";
+            content.type = "swap";
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
