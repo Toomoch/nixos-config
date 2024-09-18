80596c5306265f86f8594fecfb0dceb48df18be9
Toomoch <vallsfustearnau@gmail.com>
2024-09-18 22:48:28 +0200

    u2f auth

--- system/modules/sway.nix
+++ system/modules/sway.nix
@@ -1,124 +1,122 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.desktop;
-
-  greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
-    xwayland disable
-    input "type:touchpad" {
-      tap enabled
-    }
-    exec "${config.programs.regreet.package}/bin/regreet; swaymsg exit"
-    include /etc/sway/config.d/*
-  '';
-
 in
 {
   options.desktop = {
-    sway.enable = lib.mkEnableOption ("Whether to enable Sway with GTKgreet");
-    hyprland.enable = lib.mkEnableOption ("Whether to enable Hyprland");
-    regreet.enable = lib.mkEnableOption ("Whether to enable regreet");
+    sway.enable = lib.mkEnableOption "Whether to enable Sway with GTKgreet";
+    hyprland.enable = lib.mkEnableOption "Whether to enable Hyprland";
+    regreet.enable = lib.mkEnableOption "Whether to enable regreet";
+    tuigreet.enable = lib.mkEnableOption "Whether to enable tuigreet";
   };
 
   config = lib.mkMerge [
-    (lib.mkIf cfg.regreet.enable {
-      xdg.portal = {
-        enable = cfg.sway.enable;
-        # gtk portal needed to make gtk apps happy
-        extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-      };
+    (lib.mkIf cfg.sway.enable
+      {
+        xdg.portal = {
+          enable = cfg.sway.enable;
+          # gtk portal needed to make gtk apps happy
+          extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+        };
 
-      programs.hyprland.enable = cfg.hyprland.enable;
+        programs.hyprland.enable = cfg.hyprland.enable;
 
-      # Sway
-      programs.sway.enable = cfg.sway.enable;
-      programs.sway.wrapperFeatures.gtk = cfg.sway.enable;
-      programs.sway.extraOptions = [
-        "--unsupported-gpu"
-      ];
-
-      programs.dconf.enable = true;
-      environment.systemPackages = with pkgs; [
-        wayland
-        xorg.xwininfo
-        sway
-        adw-gtk3
-        gnome.adwaita-icon-theme
-        waypipe
-      ];
-
-      # Gnome Keyring
-      services.gnome.gnome-keyring.enable = true;
-      security.pam.services.greetd.enableGnomeKeyring = true;
-      programs.thunar = {
-        enable = true;
-        plugins = with pkgs.xfce; [
-          thunar-archive-plugin
-          thunar-volman
-          thunar-media-tags-plugin
+        # Sway
+        programs.sway.enable = true;
+        programs.sway.wrapperFeatures.gtk = true;
+        programs.sway.extraOptions = [
+          "--unsupported-gpu"
         ];
-      };
-      programs.file-roller.enable = true;
-      # Enable wayland in electron apps
-      environment.sessionVariables.NIXOS_OZONE_WL = "1"; #Disabled because of https://github.com/microsoft/vscode/issues/184124
-      # Enable wayland in firefox
-      environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
-      # Fix Java apps in WMs
-      environment.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
 
-      # For Gnome Disks
-      services.udisks2.enable = true;
+        programs.dconf.enable = true;
+        environment.systemPackages = with pkgs; [
+          wayland
+          xorg.xwininfo
+          sway
+          adw-gtk3
+          gnome.adwaita-icon-theme
+          waypipe
+        ];
 
-      # For auto mounting in Nautilus and Thunar
-      services.gvfs.enable = true;
-      services.dbus.enable = true;
+        # Gnome Keyring
+        services.gnome.gnome-keyring.enable = true;
+        security.pam.services.greetd.enableGnomeKeyring = true;
+        programs.thunar = {
+          enable = true;
+          plugins = with pkgs.xfce; [
+            thunar-archive-plugin
+            thunar-volman
+            thunar-media-tags-plugin
+          ];
+        };
+        programs.file-roller.enable = true;
+        # Enable wayland in electron apps
+        environment.sessionVariables.NIXOS_OZONE_WL = "1"; #Disabled because of https://github.com/microsoft/vscode/issues/184124
+        # Enable wayland in firefox
+        environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
+        # Fix Java apps in WMs
+        environment.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
 
-      # Don’t shutdown when power button is short-pressed
-      services.logind.extraConfig = ''
-        HandlePowerKey=ignore
-      '';
+        # For Gnome Disks
+        services.udisks2.enable = true;
 
-      # Stolen from https://github.com/sjcobb2022/nixos-config/blob/aa74d65ebb9ec49316b1f3a693176ae37381712e/hosts/common/optional/greetd.nix
-      services.greetd = {
-        enable = true;
-        settings = {
-          default_session = {
-            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd sway";
-            user = "greeter";
+        # For auto mounting in Nautilus and Thunar
+        services.gvfs.enable = true;
+        services.dbus.enable = true;
+
+        # Don’t shutdown when power button is short-pressed
+        services.logind.extraConfig = ''
+          HandlePowerKey=ignore
+        '';
+        services.blueman.enable = true;
+
+        security.pam.services.gtklock = { };
+      })
+    (lib.mkIf
+      cfg.regreet.enable
+      {
+        programs.regreet = {
+          enable = true;
+          settings = {
+            background = {
+              fit = "Cover";
+              path = ../assets/lockscreen.png;
+            };
+            GTK = {
+              cursor_theme_name = "Adwaita";
+              font_name = "Rubik 12";
+              icon_theme_name = "Adwaita";
+              theme_name = "adw-gtk3-dark";
+              application_prefer_dark_theme = true;
+            };
           };
         };
-      };
-
-      systemd.services.greetd.serviceConfig = {
-        Type = "idle";
-        StandardInput = "tty";
-        StandardOutput = "tty";
-        StandardError = "journal"; # Without this errors will spam on screen
-        # Without these bootlogs will spam on screen
-        TTYReset = true;
-        TTYVHangup = true;
-        TTYVTDisallocate = true;
-      };
-
-      programs.regreet = {
-        enable = false;
-        settings = {
-          background = {
-            fit = "Cover";
-            path = ../assets/lockscreen.png;
-          };
-          GTK = {
-            cursor_theme_name = "Adwaita";
-            font_name = "Rubik 12";
-            icon_theme_name = "Adwaita";
-            theme_name = "adw-gtk3-dark";
-            application_prefer_dark_theme = true;
+      })
+    (lib.mkIf
+      cfg.tuigreet.enable
+      {
+        # Stolen from https://github.com/sjcobb2022/nixos-config/blob/aa74d65ebb9ec49316b1f3a693176ae37381712e/hosts/common/optional/greetd.nix
+        services.greetd = {
+          enable = true;
+          settings = {
+            default_session = {
+              command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+              user = "greeter";
+            };
           };
         };
-      };
 
-      services.blueman.enable = true;
-
-      security.pam.services.gtklock = { };
-    })
+        systemd.services.greetd.serviceConfig = {
+          Type = "idle";
+          StandardInput = "tty";
+          StandardOutput = "tty";
+          StandardError = "journal"; # Without this errors will spam on screen
+          # Without these bootlogs will spam on screen
+          TTYReset = true;
+          TTYVHangup = true;
+          TTYVTDisallocate = true;
+        };
+      }
+    )
   ];
 }
--- system/users/arnau.nix
+++ system/users/arnau.nix
@@ -7,15 +7,25 @@
     extraGroups = [ "networkmanager" "wheel" "adbusers" "libvirtd" "docker" "dialout" ];
     packages = with pkgs; [ ];
     initialHashedPassword = "$y$j9T$B3GNXEDtu.tLypNHqtugL1$0TLc8R/9W0PRyTz9XCS43gbj/Fig9r2GoWyxoVdNdZ.";
-    openssh.authorizedKeys.keyFiles = [ 
+    openssh.authorizedKeys.keyFiles = [
       "${inputs.private}/secrets/keys.pub"
     ];
     shell = pkgs.zsh;
   };
   programs.zsh.enable = true;
 
+  # sudo sshAgentAuth
   security.pam.sshAgentAuth.enable = true;
-  security.pam.services.sudo.sshAgentAuth = true;
+  security.pam.services.sudo = { sshAgentAuth = true; u2fAuth = true; };
+  security.pam.services.login.u2fAuth = true;
+  security.pam.services.greetd.u2fAuth = true;
+  security.pam.u2f = {
+    enable = true;
+    cue = true;
+    origin = "pam://arnau";
+  };
+
+
   #security.pam.services.arnau.sshAgentAuth = true;
   # for unstable: (check https://github.com/NixOS/nixpkgs/issues/31611)
   # security.pam.sshAgentAuth.authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
