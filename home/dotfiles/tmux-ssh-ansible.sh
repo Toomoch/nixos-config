1d66b27acd5c06f4e44c2ab9cea0f61f4b129657
Toomoch <vallsfustearnau@gmail.com>
2025-04-13 23:23:00 +0200

    ssh scripts

--- home/dotfiles/tmux-ssh-ansible.sh
+++ home/dotfiles/tmux-ssh-ansible.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
-# Define the jq command (makes it reusable)
-JQ_CMD='._meta.hostvars as $hostvars | [to_entries[] | select(.key != "_meta" and .key != "all") | .value.hosts[] as $hostname | ($hostvars[$hostname].ansible_host) as $ansiblehost | ($ansiblehost // $hostname) as $target | (if $ansiblehost != null and $ansiblehost != $hostname then "\($hostname) (ansible_host: \($ansiblehost))" else $hostname end) as $display | "\($display)#\($target)"] | unique[]'
 
-# Run fzf, selecting based on display text, then extract the target value
-selected_target=$(
+JQ_CMD='._meta.hostvars as $hostvars |
+[to_entries[] |
+select(.key != "_meta" and .key != "all") |
+.value.hosts[] as $hostname |
+($hostvars[$hostname].ansible_host) as $ansiblehost |
+($ansiblehost // $hostname) as $target |
+(if $ansiblehost != null and $ansiblehost != $hostname then "\($hostname)-->\($ansiblehost)" else $hostname end) as $display |
+"\($display) \($target)"] | unique[]'
+
+readarray -t selected_target < <(
         ansible-inventory -i inventory.yaml --list --export |
                 jq -r "$JQ_CMD" |
-                fzf --delimiter='#' --with-nth=1 --multi --tmux --border --prompt="Select target: " |
-                awk -F '#' '{print $2}'
+                fzf --delimiter=' ' --with-nth=1 --multi --tmux --border --prompt="Target(s): " |
+                awk -F ' ' '{print $2}'
 )
+# fzf delimiters dont work https://github.com/junegunn/fzf/issues/2154
 
-printf '%s\n' "${selected_target[@]}"
-# # Check if something was selected and use it
-# if [[ -n "$selected_target" ]]; then
-#         echo "You selected target: $selected_target"
-#         # ssh "$selected_target" # Or whatever you want to do with it
-# else
-#         echo "No target selected."
-# fi
+for host in "${selected_target[@]}"; do
+        if [ -n "$host" ]; then
+                tmux new-window -n "ssh:${host}" "ssh '${host}'; tmux set-option -w automatic-rename on && exec $SHELL"
+        fi
+done
+
+exit 0
