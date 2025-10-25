7190a3fb50aab4bbb4d9860d8f2b0876a62f24d1
Toomoch <vallsfustearnau@gmail.com>
2025-10-25 23:06:35 +0200

    metrics working

--- hosts/ampere/metrics.nix
+++ hosts/ampere/metrics.nix
@@ -64,6 +64,7 @@ in
     retentionPeriod = "1y";
     extraOptions = [
       "-enableTCP6"
+      "-vmalert.proxyURL=http://localhost${toString config.services.vmalert.settings."httpListenAddr"}"
     ];
     prometheusConfig.scrape_configs = autogenScrapeConfigs;
   };
@@ -71,7 +72,23 @@ in
     enable = true;
     rules = { };
     settings = {
-      "datasource.url" = "http://localhost:${toString (lib.removePrefix ":" config.services.victoriametrics.listenAddress)}";
+      "datasource.url" =
+        "http://localhost:${toString (lib.removePrefix ":" config.services.victoriametrics.listenAddress)}";
+      "httpListenAddr" = ":8880";
+    };
+  };
+  # networking.firewall.allowedTCPPorts = [
+  #   (lib.toInt (lib.removePrefix ":" config.services.victoriametrics.listenAddress))
+  # ];
+
+  services.caddy = {
+    enable = true;
+    virtualHosts = {
+      "metrics.avalls.dev" = {
+        extraConfig = ''
+          reverse_proxy localhost:${toString (lib.removePrefix ":" config.services.victoriametrics.listenAddress)}
+        '';
+      };
     };
   };
 
