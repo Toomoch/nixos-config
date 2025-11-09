1ca667ef21a0665964f9e5eea0524a994a0cbe7f
Toomoch <vallsfustearnau@gmail.com>
2025-11-09 23:54:21 +0100

    disable stub resolver to not confict with coredns

--- hosts/rpi3/default.nix
+++ hosts/rpi3/default.nix
@@ -98,7 +98,10 @@ in
   #  })
   #];
 
-  services.resolved.enable = false;
+  # Disable stub resolver to not conflict with coredns
+  services.resolved.extraConfig = ''
+    DNSStubListener=no
+  '';
 
   services.coredns = {
     enable = true;
